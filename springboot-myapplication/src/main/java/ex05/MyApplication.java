c4e76cfdcf82cdf7b812d4c844afb459f815209f
mmmtobezip <mmmszip@gmail.com>
2024-06-25 10:44:03 +0900

    @SpringBootApplication #finish

---
+++ springboot-myapplication/src/main/java/ex05/MyApplication.java
@@ -0,0 +1,12 @@
+package ex05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MyApplication {
+  public static void main(String[] args) {
+    ApplicationContext ac = SpringApplication.run(MyApplication.class, args);
+  }
+}
---
+++ springboot-myapplication/src/main/java/ex05/component/MyComponent.java
@@ -0,0 +1,11 @@
+package ex05.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyComponent {
+
+  public void printHello() {
+    System.out.println("Hello");
+  }
+}
